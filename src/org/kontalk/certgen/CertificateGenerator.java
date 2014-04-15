d32a5f2b40e5e40c06b8b02aca95902c07160c12
Daniele Ricci <daniele.athome@gmail.com>
2014-04-15 19:17:01 +0200

    Borrow some source code from Android client
    Just depending on Bouncy Castle and building without errors.
    
    Signed-off-by: Daniele Ricci <daniele.athome@gmail.com>

---
+++ src/org/kontalk/certgen/CertificateGenerator.java
@@ -0,0 +1,13 @@
+package org.kontalk.certgen;
+
+
+/**
+ * Certificate generator entry point.
+ * @author Daniele Ricci
+ */
+public class CertificateGenerator {
+
+    public static void main(String[] args) {
+    }
+
+}
